53fae2d9b39faf3a3c83cc80492fda3ed4222054
William Debruin <wdebruin@Williams-MacBook-Pro-6.local>
2018-12-04 20:16:44 -0500

    will 2

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 var root_url = "http://comp426.cs.unc.edu:3001/";
 var user_id = "";
 var review_mode = true;
-flights = {};
+var api_key = "AIzaSyAe_sC3y9S_PQW_N_S7ccNDhq-QwmYvD1k";
+var map;
+var flights = [];
+var cur_flight_id = 0;
+var flightPath = null;
+var startMarker = null;
+var endMarker = null;
+var flightMarker = null;
+
+var depLatLng = null;
+var arrLatlng = null;
+
 
 function addFlight (){
     
@@ -210,18 +221,20 @@ function getAirportId(code){
 
 // FLITER + SEARCH FLIGHTS
 function searchFlights(){
+    
     let filter_object = {};
     $('#flight-search-msg').empty();
     
     let dep_a_id = 0;
     let arr_a_id = 0;
-    let dep_date = '';
-    let missing_param = false;
+    let dep_date = null;
+    let bad_param = false;
             
     let getInstances = (params, flight_date) => {
-        let instances = {};
+        let instances = [];
         
         let instanceExists = (flight) => {
+            rv = false;
             $.ajax(root_url + 'instances',{
                 type:'GET',
                 dataType:'json',
@@ -235,15 +248,11 @@ function searchFlights(){
                 async: false,
                 success: (response) => {
                     if(response.length > 0){
-                        return true;
-                    }else{
-                        return false;
+                        rv = true;
                     }
                 },
-                error: ()=>{
-                    return false;
-                }
             });
+            return rv;
         }
         
         $.ajax(root_url + 'flights',{
@@ -260,7 +269,7 @@ function searchFlights(){
                 }
                 for(let i=0; i<response.length; i++){
                     if(instanceExists(response[i])){
-                        instances[response[i].id] = response[i];
+                        instances.push(response[i]);
                     }
                 }
             }
@@ -268,45 +277,261 @@ function searchFlights(){
         
         return instances;
     }
+        
     let flight_number = $('#flight-number-input').val();
     if(flight_number!=''){
         filter_object['number'] = flight_number;
     }
+    
     let dep_airport = $('#departure-airport-input').val();
     if(dep_airport!=''){
         dep_a_id = getAirportId(dep_airport);
         if(dep_a_id == -1){
             $('#flight-search-msg').append('Departure airport not found. Please enter a new departure airport and try again.<br>');
-            missing_param = true;
+            bad_param = true;
         }
-        filter_object['departure_id'] = dep_a_id;
-    }else{
-        missing_param = true;
-        $('#flight-search-msg').append('Please enter a departure airport and try again.<br>');
+        filter_object['departure_id'] = dep_a_id.toString();
     }
+    
     let arr_airport = $('#arrival-airport-input').val();
     if(arr_airport!=''){
         arr_a_id = getAirportId(arr_airport);
         if(arr_a_id==-1){
             $('#flight-search-msg').append('Arrival airport not found. Please enter a new arrival airport and try again.<br>');
-            missing_param = true;
+            bad_param = true;
         }
-        filter_object['arrival_id'] = arr_a_id;
-    }else{
-        $('#flight-search-msg').append('Please enter an arrival airport and try again.<br>');
-        missing_param = true;
-    }   
-    dep_date = $('#departure-date-input').val();
-    if(dep_date == ''){
-        $('#flight-search-msg').append('Please enter a date and try again.<br>');
-        missing_param = true;
+        filter_object['arrival_id'] = arr_a_id.toString();
     }
-    if(missing_param){
+        
+    dep_date = $('#departure-date-input').val();
+    
+    if(bad_param){
         return;
     }
-    flights = getInstances(filter_object, dep_date)
+    
+    flights = getInstances(filter_object, dep_date);
+    console.log(flights.length);
+    
+    buildReviewFlightInterface();
 }
 
+function updateMap(){
+    
+    if(flightMarker==null ||depLatLng==null||arrLatlng==null){
+        return;
+    }
+    
+    let startPoint = new google.maps.LatLng(depLatLng.lat, depLatLng.lng);
+    let endPoint = new google.maps.LatLng(arrLatlng.lat, arrLatlng.lng); 
+
+    let progress = $('#flight-range').val();
+    let progress_frac = parseFloat(progress)/1000;
+    
+    var pos = google.maps.geometry.spherical.interpolate(startPoint, endPoint, progress/1000);
+    
+    flightMarker.setPosition(pos);
+    
+    let progress_perc = (progress_frac * 100).toFixed(1);
+    
+    $('#flight-range-label').html('Flight percentage completed: ' + progress_perc + '%');
+}
+
+function addNewMapPath(dep_lat, dep_lon, arr_lat, arr_lon){
+    debugger;
+    
+    if(flightPath!=null){
+        flightPath.setMap(null);
+    }
+    
+    if(startMarker!=null){
+        startMarker.setMap(null);
+    }
+    
+    if(endMarker!=null){
+        endMarker.setMap(null);
+    }
+    
+    if(flightMarker!=null){
+        flightMarker.setMap(null);
+    }
+    
+    let flightPathCoordinates = [
+        {lat: parseFloat(dep_lat), lng: parseFloat(dep_lon)},
+        {lat: parseFloat(arr_lat), lng: parseFloat(arr_lon)}
+    ];
+    flightPath = new google.maps.Polyline({
+        path: flightPathCoordinates,
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+    flightPath.setMap(map);
+    
+    depLatLng = {
+        lat: parseFloat(dep_lat),
+        lng: parseFloat(dep_lon)
+    }
+    
+    startMarker = new google.maps.Marker({
+        position: depLatLng,
+        title:"Departure Position"
+    });
+    
+    startMarker.setMap(map);
+    
+    arrLatlng = {
+        lat: parseFloat(arr_lat),
+        lng: parseFloat(arr_lon)
+    }
+    
+    endMarker = new google.maps.Marker({
+       position:arrLatlng,
+        title: "Arrival Position"
+    });
+    
+    endMarker.setMap(map);
+    
+    flightMarker = new google.maps.Marker({
+       position: depLatLng,
+        title:"Flight Position"
+    });
+    
+    flightMarker.setMap(map);
+    
+    $('#flight-range').val(0);
+}
+
+function initMap(){
+    map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 41.850033, lng: -87.6500523},
+    zoom: 3
+  });
+}
+
+function appendFlightList($container){    
+    let $list_group = $('<ul>',{class:'list-group flight-review-list'});
+        
+    for(let i=0; i<flights.length; i++){
+        let elt_id = 'list-elt-' + flights[i].id;
+        let $list_elt = $('<li>',{
+            class:'list-group-item',
+            id: elt_id,
+            type:'button',
+            onClick:'trackNewFlight('+flights[i].id+')'
+        });
+        $list_group.append($list_elt);
+        $.ajax(root_url+'airlines/'+flights[i].airline_id,{
+            type:'GET',
+            dataType:'json',
+            xhrFields:{withCredentials:true},
+            success:(response)=>{
+                $('#'+elt_id).html($('#'+elt_id).html() + response.name + ' -- ');
+                $.ajax(root_url + 'airports/' + flights[i].departure_id,{
+                    type:'GET',
+                    dataType:'json',
+                    xhrFields:{withCredentials:true},
+                    success:(response)=>{
+                        $('#'+elt_id).html($('#'+elt_id).html() + response.code +'->');
+                        $.ajax(root_url + 'airports/' + flights[i].arrival_id,{
+                            type:'GET',
+                            dataType:'json',
+                            xhrFields:{withCredentials:true},
+                            success:(response)=>{
+                                $('#'+elt_id).html($('#'+elt_id).html() + response.code);
+                            }
+                        });
+                    }    
+                });
+            }
+        });
+    }
+    
+    $container.append($list_group);
+}
+
+function trackNewFlight(flight_id){
+    debugger;
+    
+    if(flight_id == cur_flight_id){
+        return;
+    }
+    
+    let flight = null;
+    
+    for(let i=0; i<flights.length; i++){
+        if(flights[i].id == flight_id){
+            flight = flights[i];
+            break;
+        }
+    }
+    
+    if(flight==null){
+        return;
+    }
+    
+    $('#list-elt-' + cur_flight_id).removeClass('active');
+    cur_flight_id = flight_id;
+    
+    let $flight_li = $('#list-elt-'+flight_id);
+    $flight_li.addClass('active');
+    
+    $.ajax(root_url + 'airports/' + flight.departure_id,{
+        type:'GET',
+        dataType:'json',
+        xhrFields:{withCredentials:true},
+        success:(response)=>{
+            let dep_lat = response.latitude;
+            let dep_lon = response.longitude;
+            $.ajax(root_url+'airports/'+flight.arrival_id,{
+                type:'GET',
+                dataType:'json',
+                xhrFields:{withCredentials:true},
+                success: (response)=>{
+                    let arr_lat = response.latitude;
+                    let arr_lon = response.longitude;
+                    addNewMapPath(dep_lat, dep_lon, arr_lat, arr_lon);
+                }
+            });
+        }
+        
+    });
+    
+}
+
+function buildReviewFlightInterface(){
+    let $body = $('body');
+    $body.empty();
+    
+    appendNavBar($body);
+    
+    
+    let $jumbo = $('<div>', {class:'jumbotron container container-fluid'});
+        
+    let $row = $('<div>', {class:'row'});
+    let $col1 = $('<div>', {class:'col-xs-12 col-sm-12 col-md-4 col-lg-4', id:'flight-review-list-col'});
+    
+    $col1.append('<h4>Select Flight To Review</h4>');
+    $col1.append('<p>Click on the flight you would like to visualize on the map</p>');
+    appendFlightList($col1);
+    $col1.append($('<button>',{class:'btn btn-primary btn-block', html:'Change Search Criteria', onclick:'buildReviewSelectorInterface()'}));
+    
+    $row.append($col1);
+    
+    let $col2 = $('<div>',{class: 'col-xs-12 col-sm-12 col-md-8 col-lg-8', id:'map-col'});
+    $col2.append($('<div>', {class:'container container-fluid', id:'map'}));
+    $col2.append($('<label>',{for:'flight-range', html:'Flight percentage completed:', id:'flight-range-label'}));
+    $col2.append($('<input>',{type:'range', class:'custom-range', id:'flight-range', min: 0, max:1000, oninput:'updateMap()'}));
+    
+    $row.append($col2);
+    
+    $jumbo.append($row);
+    $body.append($jumbo);
+    
+    debugger;
+    
+    initMap();
+}
 
 // NEW USER
 function create_user (user, password){
